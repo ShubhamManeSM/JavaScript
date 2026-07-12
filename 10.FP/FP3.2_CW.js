c94d11f9705d4b3eed8d0fad4a5a626c0ccb2e63
Shubham Mane <shubhammane7096@gmail.com>
2026-07-12 19:37:44 +0530

    Added Functional Programming Examples

---
+++ 10.FP/FP3.2_CW.js
@@ -0,0 +1,34 @@
+// array of obj into an obj
+const data = [
+  {key: "name", value: "John"},
+  {key: "age", value: 25},
+  {key: "city", value: "London"}
+]
+
+const transformedData = data.reduce((acc, curr) => {
+  acc[curr.key] = curr.value
+  return acc
+}, {})
+console.log(transformedData)
+
+
+// nested array into a single array
+const nestedArray = [[1,2],[3,4],[4,5],[5,6]]
+// [1,2,3,4,5,6]
+
+const flattenedArray = nestedArray.reduce((acc, curr) => acc.concat(curr), [])
+console.log(flattenedArray)
+
+
+// array of objects into an array
+const students = [
+  {name: "Alice", grade: "A"},
+  {name: "Bob", grade: "B"},
+  {name: "John", grade: "C"}
+]
+
+const grades = students.reduce((acc, curr) => {
+  acc.push(curr.grade)
+  return acc
+}, [])
+console.log(grades)
