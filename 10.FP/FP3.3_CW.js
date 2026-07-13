086d9a6e80980d4d14b6b67fd7ae9d2a70e6dc4f
Shubham Mane <shubhammane7096@gmail.com>
2026-07-13 09:06:07 +0530

    Added Functional Programming Examples

---
+++ 10.FP/FP3.3_CW.js
@@ -0,0 +1,24 @@
+const movies = [
+  {id: 1, title: "Movie 1", rating: 4.5},
+  {id: 2, title: "Movie 2", rating: 3.8},
+  {id: 3, title: "Movie 3", rating: 4.2},
+  {id: 4, title: "Movie 4", rating: 3.5}
+]
+
+const movieListContainer = document.querySelector("#moviesList")
+const averageRatingContainer = document.querySelector("#averageRating")
+
+const movieList = movies.reduce((acc,curr) => {
+  acc.totalRating = acc.totalRating + curr.rating
+
+  // display the movie list
+  const listItem = document.createElement("li")
+  listItem.textContent = `${curr.title} - Rating: ${curr.rating}`
+  movieListContainer.appendChild(listItem)
+  
+  return acc
+}, {totalRating: 0})
+
+const averageRating = movieList.totalRating / movies.length
+
+averageRatingContainer.textContent = averageRating
